5c1f1d5728b584ef7c081d3b044ce7f86d7c2f8b
Amey2902 <amey020607@gmail.com>
2026-07-24 11:31:46 +0530

    Remove Railway-specific files (docker-entrypoint.sh, railway.toml)

--- devshowcase_frontend/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/sh
-# Railway provides $PORT â€” substitute it in the nginx config
-PORT=${PORT:-80}
-sed -i "s/listen 80;/listen $PORT;/" /etc/nginx/conf.d/default.conf
-exec nginx -g "daemon off;"
